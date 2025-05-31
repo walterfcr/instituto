d53f01c6fd6135d1324faba84c77068b1551556b
walterfcr <143011753+walterfcr@users.noreply.github.com>
2025-05-31 10:24:00 -0600

    improve news

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+  const form = document.getElementById('contact-form');
+  form.addEventListener('submit', function (event) {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+  });
